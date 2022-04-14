4a0bd256d543dc35f1f184c186faec3ceefce7d7
mirror42 <34626569+noppytinto@users.noreply.github.com>
2022-04-14 14:09:38 +0200

    bugfix

--- src/index.js
+++ src/index.js
@@ -8,12 +8,13 @@ import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/htmlmixed/htmlmixed';
 import 'codemirror/mode/css/css';
 import CodeMirror from 'codemirror';
+import * as codeEditor from './scripts/editor-service';
+const [htmlEditor, cssEditor, jsEditor] = codeEditor.init(CodeMirror);
 
 import * as configService from './scripts/services/config-service';
 configService.setAppMode('development');
 
 import * as uiService from './scripts/services/ui-service';
-import * as codeEditor from './scripts/editor';
 import * as session from './scripts/session';
 import * as adminService from './scripts/services/admin-service';
 import './styles/style.scss';
@@ -37,7 +38,6 @@ const pageData = {
     cssCode: previousSessionData.cssCode ?? '',
     jsCode: previousSessionData.jsCode ?? '',
 }
-const [htmlEditor, cssEditor, jsEditor] = codeEditor.init(CodeMirror);
 
 
 
@@ -47,12 +47,13 @@ const [htmlEditor, cssEditor, jsEditor] = codeEditor.init(CodeMirror);
 uiService.checkMediaQuery()
 uiService.handleTabsClick();
 uiService.handleResizer();
-adminService.authorizeApp(() => {
-    console.log('APP AUTHORIZED');
-    uiService.restorePreviousViewState(pageData, {htmlEditor, cssEditor, jsEditor});
-    uiService.listenEditorsChanges(pageData, {htmlEditor, cssEditor, jsEditor});
-});
 
+// adminService.authorizeApp(() => {
+//     console.log('APP AUTHORIZED');
+//     uiService.restorePreviousViewState(pageData, {htmlEditor, cssEditor, jsEditor});
+//     uiService.listenEditorsChanges(pageData, {htmlEditor, cssEditor, jsEditor});
+// });
+//
 
 
 /////////////////////////////////////////
