1a9c4dc0de1a4403ca8298f37e4b0772cf4758a6
mirror42 <34626569+noppytinto@users.noreply.github.com>
2022-04-16 12:00:46 +0200

    fix url

--- src/scripts/services/admin-service.js
+++ src/scripts/services/admin-service.js
@@ -2,9 +2,9 @@ import * as configService from './config-service';
 
 
 export function authorizeApp(onSuccess, onFailed) {
-    let url = 'http://localhost:3000/admin/authorize-app';
+    let url = 'http://localhost:3000/auth/authorize-app';
     if (configService.isProductionMode()) {
-        url = 'https://noppytinto-web-playground.herokuapp.com/admin/authorize-app';
+        url = 'https://noppytinto-web-playground.herokuapp.com/auth/authorize-app';
     }
 
     const request = new Request(url, {
