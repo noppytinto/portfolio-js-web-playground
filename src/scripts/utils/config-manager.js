c0fd45a2269e920088819e9fb8f4baaf803fe949
mirror42 <34626569+noppytinto@users.noreply.github.com>
2022-04-21 19:49:26 +0200

    modify some params

--- src/scripts/utils/config-manager.js
+++ src/scripts/utils/config-manager.js
@@ -4,7 +4,7 @@ let appMode = DEVELOPMENT;
 
 
 function getAppToken() {
-    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IlJPckdQdHpSMmJqTk5keVVteDY2b1dndENjZVE3Y2VzYlFMY2Jzb3hyZzlHdDNpd1lCIiwiaWF0IjoxNjUwNTM1MDk5LCJleHAiOjE2NTA1MzU5OTl9.rbaEqIgEuJtq5aWMWHmZvX2s5ppXnpmkKZzAN6WdsLU';
+    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IlJPckdQdHpSMmJqTk5keVVteDY2b1dndENjZVE3Y2VzYlFMY2Jzb3hyZzlHdDNpd1lCIiwiaWF0IjoxNjUwNTYzMjg5LCJleHAiOjE2ODIxMjA4ODl9.qpH-Trp90rK2vVz1YxPPX-zsiXk78H_1LaF6F_m5SFw';
 }
 
 function isDevelopmentMode() {
